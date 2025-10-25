67f3cc31587f4f764b09a96862cdfd534806cee3
EduardoTartas <eduardos.tartas@gmail.com>
2025-10-24 22:18:45 -0400

    adiciona react-loading-indicators e substitui loader na página de eventos

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -21,16 +21,6 @@ export default function AuthLayout({
     }
   }, [status, router]);
 
-  // Enquanto o estado da sessão estiver carregando, você pode mostrar um loader
-  if (status === "loading") {
-    return (
-      <div className="bg-linear-to-br from-indigo-50 to-blue-100 flex items-center justify-center h-screen">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600"></div>
-        <span className="ml-3 text-gray-600">Carregando ...</span>
-      </div>
-    );
-  }
-
   // Se está autenticado, renderiza os filhos
   if (status === "authenticated") {
     return (
